c3e12e0c9e8105a78a608aa4f9a2cf79c0294159
Rick Smith <ricochet2200@gmail.com>
2013-05-17 13:35:16 -0600

    TODO: move client, msg, and server to 3 repositories

--- client.go
+++ client.go
@@ -8,9 +8,9 @@ import (
 
 const ServerHost = "localhost:8022"
 
-func main() {
+/*func main() {
 	client := client.NewClient(ServerHost, "user", "password")
 
 	ip, port, err := client.Bind()
 	log.Println(ip, port, err)
-}
+}*/
